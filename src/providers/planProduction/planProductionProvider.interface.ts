854108b3f9a23b0b60e173df74536049d78686b8
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2026-09-02 20:56:10 +0700

    refactor  (providers) : deleted unused plan production providers

--- src/providers/planProduction/planProductionProvider.interface.ts
+++
@@ -1,5 +0,0 @@
-import {PlanProductionInfo} from "../../types/existingSystem.types";
-
-export interface IPlanProductionProvider {
-  getActivePlan(scaleArea: string, date: Date): Promise<PlanProductionInfo | null>;
-}
